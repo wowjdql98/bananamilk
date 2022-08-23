c06f3de0795bd260eef61ebd87a17ad7a18f11b3
hyun_jin98 <105402290+wowjdql98@users.noreply.github.com>
2022-08-23 13:59:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-$(function(){
+$(function () {
 
-	$("#toggle-btn").click(function(){
+	$("#toggle-btn").click(function () {
 		$("#main-header nav").toggleClass("on");
 	});
 
-
 	// 모달창
-	$(".youtube").click(function(){
-		$(".modal").addClass("on");
-	})
-	$(".btn-close").click(function(){
-		$(".modal").removeClass("on");
-	})
+	//열기
+	$('#main-section2 .youtube').click(function () {
+		$('.modal').addClass('on')
+	});
+
+	//닫기
+	$('.box .btn-close').click(function () {
+		$('.modal').removeClass('on')
+	});
 
 }); // $
 
 
-//이미지 및 컨첸츠 로딩 체크 -윈도우가 로딩다되면 초기화
-window.onload = function(){
-		// AOS 초기화
-		AOS.init();
+// 이미지 및 콘텐츠 로딩 채크
+window.onload = function() {
+	// AOS 초기화
+	AOS.init();
 }
